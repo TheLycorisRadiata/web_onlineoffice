f28ce8d24354965283bd04cfe9798b7b87afd0ad
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2021-06-02 17:53:05 +0200

    Add logos

--- js/Contact.js
+++ js/Contact.js
@@ -13,22 +13,6 @@ function Contact(lastname, firstname, id, email, phone, is_active)
 	}
 }
 
-function ContactPerso(lastname, firstname, id, email, phone, is_active, facebook)
-{
-	Contact.call(this, lastname, firstname, id, email, phone, is_active);
-	this.link = facebook;
-
-	this.present_contact = function()
-	{
-		const do_redirect = confirm(`Hello! My name is ${this.firstname} ${this.lastname}. You can contact me at ${this.email} or on ${this.phone}.` + 
-			`\r\n\r\n` + 
-			`You can also find me on Facebook! Click on "OK" to be redirected to my account.`);
-
-		if (do_redirect)
-			open(this.link, '_blank');
-	}
-}
-
 function ContactPro(lastname, firstname, id, email, phone, is_active, linkedin)
 {
 	Contact.call(this, lastname, firstname, id, email, phone, is_active);
@@ -39,6 +23,22 @@ function ContactPro(lastname, firstname, id, email, phone, is_active, linkedin)
 		const do_redirect = confirm(`Hello! My name is ${this.firstname} ${this.lastname}. You can contact me at ${this.email} or on ${this.phone}.` + 
 			`\r\n\r\n` + 
 			`You can also find me on LinkedIn! Click on "OK" to be redirected to my account.`);
+
+		if (do_redirect)
+			open(this.link, '_blank');
+	}
+}
+
+function ContactPerso(lastname, firstname, id, email, phone, is_active, facebook)
+{
+	Contact.call(this, lastname, firstname, id, email, phone, is_active);
+	this.link = facebook;
+
+	this.present_contact = function()
+	{
+		const do_redirect = confirm(`Hello! My name is ${this.firstname} ${this.lastname}. You can contact me at ${this.email} or on ${this.phone}.` + 
+			`\r\n\r\n` + 
+			`You can also find me on Facebook! Click on "OK" to be redirected to my account.`);
 
 		if (do_redirect)
 			open(this.link, '_blank');
