2b5ee13e2510c6c0fa6d5bf95f059ed1edafd43f
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2021-06-12 21:33:54 +0200

    Add Address Book to the local storage + a bit of CSS

--- js/addressbook/Contact.js
+++ js/addressbook/Contact.js
@@ -10,9 +10,9 @@ class Contact
 		this.is_active = is_active;
 	}
 
-	present_contact()
+	static present_contact(contact)
 	{
-		alert(`Hello! My name is ${this.firstname} ${this.lastname}. You can contact me at ${this.email} or on ${this.phone}.`);
+		alert(`Hello! My name is ${contact.firstname} ${contact.lastname}. You can contact me at ${contact.email} or on ${contact.phone}.`);
 	}
 }
 
@@ -24,14 +24,14 @@ class ContactPro extends Contact
 		this.link = linkedin;
 	}
 
-	present_contact()
+	static present_contact(contact)
 	{
-		const do_redirect = confirm(`Hello! My name is ${this.firstname} ${this.lastname}. You can contact me at ${this.email} or on ${this.phone}.` + 
+		const do_redirect = confirm(`Hello! My name is ${contact.firstname} ${contact.lastname}. You can contact me at ${contact.email} or on ${contact.phone}.` + 
 			`\r\n\r\n` + 
 			`You can also find me on LinkedIn! Click on "OK" to be redirected to my account.`);
 
 		if (do_redirect)
-			open(this.link, '_blank');
+			open(contact.link, '_blank');
 	}
 }
 
@@ -43,14 +43,14 @@ class ContactPerso extends Contact
 		this.link = facebook;
 	}
 
-	present_contact()
+	static present_contact(contact)
 	{
-		const do_redirect = confirm(`Hello! My name is ${this.firstname} ${this.lastname}. You can contact me at ${this.email} or on ${this.phone}.` + 
+		const do_redirect = confirm(`Hello! My name is ${contact.firstname} ${contact.lastname}. You can contact me at ${contact.email} or on ${contact.phone}.` + 
 			`\r\n\r\n` + 
 			`You can also find me on Facebook! Click on "OK" to be redirected to my account.`);
 
 		if (do_redirect)
-			open(this.link, '_blank');
+			open(contact.link, '_blank');
 	}
 }
 
