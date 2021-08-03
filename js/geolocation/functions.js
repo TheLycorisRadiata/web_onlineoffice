f8708800cc70e46def4fa2c4f71f3dac25d13567
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2021-08-03 22:31:07 +0200

    Google Maps API

--- js/geolocation/functions.js
+++ js/geolocation/functions.js
@@ -1,3 +1,14 @@
+let map, latitude, longitude;
+
+function initialize_map()
+{
+	map = new google.maps.Map(document.getElementById("map"),
+	{
+		center: { lat: latitude, lng: longitude },
+		zoom: 17,
+	});
+}
+
 function display_user_position()
 {
 	if (!user_position)
@@ -6,7 +17,9 @@ function display_user_position()
 		display_longitude.innerText = 'Longitude: 0.';
 		display_precision.innerText = 'Precision is of 0 meter.';
 
-		document.querySelector('iframe').src = 'https://maps.google.com/maps?q=40.753181,-73.982254&z=17&output=embed';
+		latitude = 40.753181;
+		longitude = -73.982254;
+		initialize_map();
 	}
 	else
 	{
@@ -14,7 +27,9 @@ function display_user_position()
 		display_longitude.innerText = `Longitude: ${user_position.longitude}.`;
 		display_precision.innerText = `Precision is of more or less ${Math.round(user_position.accuracy)} meters.`;
 
-		document.querySelector('iframe').src = `https://maps.google.com/maps?q=${user_position.latitude}, ${user_position.longitude}&z=17&output=embed`;
+		latitude = user_position.latitude;
+		longitude = user_position.longitude;
+		initialize_map();
 	}
 }
 
